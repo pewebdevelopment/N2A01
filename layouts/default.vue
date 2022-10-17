f32ed520636161c0bae3400f956ef4ff8dbd8616
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-10-17 09:54:36 +0530

    Working w/o Zen.vue layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { useStyleStore } from "@/stores/style.js";
+import { useStyleStore } from "@/store/style.js";
 const styleStore = useStyleStore();
 </script>
 
 <template>
   <div :class="{ dark: styleStore.darkMode }">
     <div class="bg-gray-50 dark:bg-slate-800 dark:text-slate-100">
-      <slot />
+      <Nuxt/>
     </div>
   </div>
 </template>
