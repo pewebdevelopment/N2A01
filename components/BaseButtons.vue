b6b1880ccc227bb8cf4fdc3ad7178ef123a7fe57
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-14 18:56:28 +0530

    Basebuttons commented

--- components/BaseButtons.vue
+++ components/BaseButtons.vue
@@ -1,55 +1,41 @@
 
 <script>
-import { h, defineComponent, } from 'vue'
+import { h, defineComponent } from 'vue'
 // import { h, defineComponent, useSlots } from 'vue'
 
 export default defineComponent({
-  name: "BaseButtons",
+  name: 'BaseButtons',
   props: {
     noWrap: Boolean,
     type: {
       type: String,
-      default: "justify-start",
+      default: 'justify-start',
     },
     classAddon: {
       type: String,
-      default: "mr-3 last:mr-0 mb-3",
+      default: 'mr-3 last:mr-0 mb-3',
     },
     mb: {
       type: String,
-      default: "-mb-3",
+      default: '-mb-3',
     },
   },
 
-
   setup(props) {
-
     // const Myslots = useSlots();
     // const hasSlot = true;
 
     const parentClass = [
-      "flex",
-      "items-center",
+      'flex',
+      'items-center',
       props.type,
-      props.noWrap ? "flex-nowrap" : "flex-wrap",
-    ];
+      props.noWrap ? 'flex-nowrap' : 'flex-wrap',
+    ]
     if (props.mb) {
-      parentClass.push(props.mb);
+      parentClass.push(props.mb)
     }
 
-    return () => h(
-      "div",
-      { class: parentClass },
-      null
-    );
-
-
-  }
-
-
-});
-</script>
-
+    return () => h('div', { class: parentClass }, null)
 
     // return () => h(
     //   "div",
@@ -73,3 +59,8 @@ export default defineComponent({
     //     })
     //     : null
     // );
+  },
+})
+</script>
+
+
