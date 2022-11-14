40d3fc4c9fe7ff6fd1af189f1c86c1fe44636013
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-14 08:47:57 +0530

    BaseLevel working again with Vue Render Functions

--- components/BaseLevel.vue
+++ components/BaseLevel.vue
@@ -1,8 +1,8 @@
 <script>
 
 // import { defineComponent } from "vue";
-import {  defineComponent } from 'vue'
-// import { useSlots } from 'vue';
+import { h, defineComponent, useSlots } from 'vue'
+// import {  } from 'vue';
 
 
 export default defineComponent({
@@ -17,26 +17,40 @@ export default defineComponent({
 
   setup(props) {
 
+    const slots = useSlots();
+
     const parentClass = [props.type, 'items-center']
 
     const parentMobileClass = ['flex']
 
     const parentBaseClass = ['block', 'md:flex']
 
-   // const slots = useSlots();
+
     // slots = slots.default ? slots.default() : [];
 
 
     const childBaseClass = ['flex', 'items-center', 'justify-center']
-    return {
-      parentBaseClass,
-      parentClass,
-      parentMobileClass,
-      childBaseClass,
-    }
+    return () => h(
+      "div",
+      {
+        class: parentClass.concat(
+          props.mobile ? parentMobileClass : parentBaseClass
+        )
+      },
+
+      slots.default().map((element, index) => {
+        const childClass =
+          !props.mobile && slots.default().length > index + 1
+            ? childBaseClass.concat(["mb-6", "md:mb-0"])
+            : childBaseClass;
+
+        return h("div", { class: childClass }, [element]);
+      })
+    )
+
+
   },
   // render() {
-
 
   // return h(
   //   'div',
@@ -56,72 +70,11 @@ export default defineComponent({
   // },
   // render() {
 
-    return () => h(
-      "div",
-      {
-        class: parentClass.concat(
-          props.mobile ? parentMobileClass : parentBaseClass
-        )
-      },
-
-      slots.default().map((element, index) => {
-        const childClass =
-          !props.mobile && slots.default().length > index + 1
-            ? childBaseClass.concat(["mb-6", "md:mb-0"])
-            : childBaseClass;
-
-        return h("div", { class: childClass }, [element]);
-      })
-    )
-  },
-
-})
-
-</script>
-  //   render() {
-
-  //   const parentClass = [this.type, "items-center"];
-
-  //   const parentMobileClass = ["flex"];
-
-  //   const parentBaseClass = ["block", "md:flex"];
-
-  //   const childBaseClass = ["flex", "items-center", "justify-center"];
-
-  //   return h(
-  //     "div",
-  //     {
-  //       class: parentClass.concat(
-  //         this.mobile ? parentMobileClass : parentBaseClass
-  //       ),
-  //     },
-  //     ((element, index) => {
-  //       const childClass =
-  //         !this.mobile 
-  //           ? childBaseClass.concat(["mb-6", "md:mb-0"])
-  //           : childBaseClass;
-
-  //       return h("div", { class: childClass }, [element]);
-  //     })
-  //   );
-  // },
 
 })
 </script>
 
-<template>
-  <div
-    :class="[
-      mobile
-        ? parentClass.concat(parentMobileClass)
-        : parentClass.concat(parentBaseClass),
-    ]"
-  >
-    <template>
-      <slot/>
-    </template>
-  </div>
-</template>
+
 
 //   render() {
 //     const parentClass = [this.type, "items-center"];
