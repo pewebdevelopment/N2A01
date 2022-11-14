d261f48d02edcb08cf80b18a77e09d2a0f67faf3
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-14 09:23:59 +0530

    Trying to resolve Basebuttons

--- components/BaseButtons.vue
+++ components/BaseButtons.vue
@@ -1,33 +1,87 @@
-<template>
-  <div
-    class="flex items-center gap-3"
-    :class="[type, mb, noWrap ? '' : 'flex-wrap']"
-  >
-    
-      <slot name="buttons" />
-    
-  </div>
-</template>
 
 <script>
-
-import { defineComponent } from 'vue'
-
-
+import { h, defineComponent, useSlots } from "vue";
 export default defineComponent({
-  name: 'BaseButtons',
+  name: "BaseButtons",
   props: {
     noWrap: Boolean,
     type: {
       type: String,
-      default: 'justify-start',
+      default: "justify-start",
+    },
+    classAddon: {
+      type: String,
+      default: "mr-3 last:mr-0 mb-3",
     },
     mb: {
       type: String,
-      default: '-mb-3',
+      default: "-mb-3",
     },
   },
 
-})
 
+  setup(props) {
+
+    const slots = useSlots();
+    const hasSlot = slots;
+
+    const parentClass = [
+      "flex",
+      "items-center",
+      props.type,
+      props.noWrap ? "flex-nowrap" : "flex-wrap",
+    ];
+    if (props.mb) {
+      parentClass.push(props.mb);
+    }
+
+    return h(
+      "div",
+      { class: parentClass },
+      hasSlot
+        ? slots.default().map((element) => {
+          if (
+            element &&
+            element.children &&
+            typeof element.children === "object"
+          ) {
+            return h(
+              element,
+              {},
+              element.children.map((child) => {
+                return h(child, { class: [props.classAddon] });
+              })
+            );
+          }
+          return h(element, { class: [props.classAddon] });
+        })
+        : null
+    );
+
+
+  }
+
+
+});
 </script>
+
+// import { defineComponent } from 'vue'
+
+
+// export default defineComponent({
+//   name: 'BaseButtons',
+//   props: {
+//     noWrap: Boolean,
+//     type: {
+//       type: String,
+//       default: 'justify-start',
+//     },
+//     mb: {
+//       type: String,
+//       default: '-mb-3',
+//     },
+//   },
+
+// })
+
+// </script>
