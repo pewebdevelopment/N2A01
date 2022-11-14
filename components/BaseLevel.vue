84884be7cca5c9843bd6b5cf6648a5f69b03fcbe
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-14 08:53:54 +0530

    Deleted comments from BaseLevel.vue

--- components/BaseLevel.vue
+++ components/BaseLevel.vue
@@ -26,9 +26,6 @@ export default defineComponent({
     const parentBaseClass = ['block', 'md:flex']
 
 
-    // slots = slots.default ? slots.default() : [];
-
-
     const childBaseClass = ['flex', 'items-center', 'justify-center']
     return () => h(
       "div",
@@ -50,64 +47,10 @@ export default defineComponent({
 
 
   },
-  // render() {
-
-  // return h(
-  //   'div',
-  //   {
-  //     class: parentClass.concat(
-  //       this.mobile ? parentMobileClass : parentBaseClass
-  //     ),
-  //   },
-  //   (element, index) => {
-  //     const childClass = !this.mobile
-  //       ? childBaseClass.concat(['mb-6', 'md:mb-0'])
-  //       : childBaseClass
-
-  //     return h('div', { class: childClass }, [element])
-  //   }
-  // )
-  // },
-  // render() {
 
 
 })
 </script>
 
 
-
-//   render() {
-//     const parentClass = [this.type, "items-center"];
-
-//     const parentMobileClass = ["flex"];
-
-//     const parentBaseClass = ["block", "md:flex"];
-
-//     const childBaseClass = ["flex", "items-center", "justify-center"];
-
-//     return h(
-//       "div",
-//       {
-//         class: parentClass.concat(
-//           this.mobile ? parentMobileClass : parentBaseClass
-//         ),
-//       },
-//       {
-//         this.$slots.default().map((element, index) => {
-//           const childClass =
-//             !this.mobile && this.$slots.default().length > index + 1
-//               ? childBaseClass.concat(["mb-6", "md:mb-0"])
-//               : childBaseClass;
-
-//           return h("div", { class: childClass }, [element]);
-//         })
-    
-//     );
-
-//   },
-// });
-
-
-
- //   if (this.$slots.default()) {}
 
