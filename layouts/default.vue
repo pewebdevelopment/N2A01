fbfc7f57eae52a2e9f0ebc6589aca61edccfd6dd
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-15 22:15:36 +0530

    default layout is working on workdesk

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,23 @@
 <script setup>
-import { useStyleStore } from "@/store/style.js";
-const styleStore = useStyleStore();
+import { useStyleStore } from '@/store/style.js'
+const styleStore = useStyleStore()
 </script>
 
 <template>
   <div :class="{ dark: styleStore.darkMode }">
-    <div class="bg-gray-50 dark:bg-slate-800 dark:text-slate-100">
-      <Nuxt/>
+    <div
+      class="
+        bg-slate-800
+        flex
+        justify-center
+        text-white
+        dark:bg-slate-800 dark:text-slate-100
+      "
+    >
+      This is the default layout
+      <div>
+        <Nuxt />
+      </div>
     </div>
   </div>
 </template>
--- pages/workdesk.vue
+++ pages/workdesk.vue
@@ -1,15 +1,16 @@
 
 
-<script setup>
-
+<script>
+export default {
+  layout: 'zen', // you can set a custom layout for the error page
+}
 </script>
 
 <script>
-
 </script>
 
 <template>
-    <div class ="bg-gray-800 flex justify-center text-white">
+  <div class="bg-blue-800 flex justify-center text-white">
     <h1>WorkDesk.vue Page</h1>
-    </div>
+  </div>
 </template>
