d21bc6460e66d66bda4b5a6332c878777a68b5ee
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-15 22:01:25 +0530

    uiview restored

--- app.vue
+++ app.vue
@@ -1,6 +1,30 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useStyleStore } from '@/stores/style.js'
+import { darkModeKey, styleKey } from '@/configs/config'
+
+// import '@/assets/css/main.css'
+
+const styleStore = useStyleStore()
+const loading = ref(true)
+
+onMounted(() => {
+  styleStore.setStyle(localStorage[styleKey] ?? 'basic')
+  /* Dark mode */
+  if (
+    (!localStorage[darkModeKey] &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches) ||
+    localStorage[darkModeKey] === '1'
+  ) {
+    styleStore.setDarkMode()
+  }
+  loading.value = false
+})
+</script>
+
+
 <template>
-    <div>
-      <Nuxt />
-      
-    </div>
-  </template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
